3fe6dfb8e2c0900622bfaba9d476bd811088f2cc
Waheed Khan <waheedkhan05@yahoo.com>
2014-05-28 11:03:09 +0500

    Replacing zip and state_id fields

--- openerp/addons/post_code/postcode.py
+++ openerp/addons/post_code/postcode.py
@@ -15,7 +15,8 @@ class res_partner(osv.osv):
 			return [('Post Codes Unavailable','Post Codes Unavailable')]
 
 	_columns = {
-		'postcode' : fields.many2one('uk_postcodes', domain="[('postcode','=',True)]"),
+		'zip' : fields.many2one('uk_postcodes',domain="[('zip','=',name)]"),
+		'state_id':fields.many2one('uk_postcodes',domain="[('state_id','=',name)]")
 	}
 
 	def onchange_postcode(self, cr, uid,ids,postcode, context=None):
@@ -25,55 +26,84 @@ class res_partner(osv.osv):
 				return {
 		        'value': {
 		            'street2': obj['town'],
-		            'city': obj['county'],
+		            'state_id': obj['state_id'],
 		            'country_id':233
 				         }
 				    }
 		return {}
+
+	def onchange_County(self, cr, uid,ids, context=None):
+		if ids:
+			obj = self.pool.get('uk_postcodes').getAllCounties(cr,uid,ids,context)
+		return {}
+
+
 res_partner()
 
 class uk_postcodes(osv.osv):
 	_name = "uk_postcodes"
 	_description = "Extending Customer Information to add Post Code Information"
 	_columns = {
-		'postcode':fields.char('Post Code',size=5),
+		'zip':fields.char('Post Code',size=5),
 		'x':fields.integer(),
 		'y':fields.integer(),
 		'latitude':fields.float('Latitude',digits=(5,2)),
 		'longitude':fields.char('Longitude',size=8),
 		'town':fields.char('Town',size=255),
-		'county':fields.char('County',size=255),
+		'state_id':fields.char('County',size=255),
 	}
 
 	def name_search(self, cr, uid, name='', args=None, operator='ilike',context=None, limit=100):
+	    ids = []
 	    if not args:
 	        args = []
-	    ids = []
-	    search_domain = [('postcode', operator, name)]
-	    # if ids: 
-	    # 	search_domain.append(('postcode', 'in', ids))
-	    ids = self.search(cr, uid, search_domain,limit=limit, context=context)
-	    return [(r.id,r.postcode) for r in self.browse(cr, uid, ids, context)]
+	    else:
+	    	if 'state_id' in args[0]:
+	    		ids = self.search(cr, uid, [('state_id',operator,name)],limit=limit, context=context)
+	    		return [(r.id,r.state_id) for r in self.browse(cr, uid, ids, context)]
+	    	elif 'zip' in args[0]:
+	    		ids = self.search(cr, uid, [('zip',operator,name)],limit=limit, context=context)
+	    		return [(r.id,r.zip) for r in self.browse(cr, uid, ids, context)]
+	    	else:
+			    print 'name_search name:%s args:%s,context:%s'%(name,args,context)
+			    print name
+		return []
+	    
 	    #return self.name_get(cr, uid, ids, context)
 
-	def name_get(self,cr, uid, ids, context):
-		#return (self.browse(cr,uid,ids)[0].id,self.browse(cr,uid,ids)[0].postcode)
-		res = []
-		for r in self.read(cr,uid,ids):
-			res.append((r['id'],r['postcode']))
-		return res
+	# def name_get(self,cr, uid, ids, context):
+	# 	#return (self.browse(cr,uid,ids)[0].id,self.browse(cr,uid,ids)[0].postcode)
+	# 	res = []
+	# 	for r in self.read(cr,uid,ids):
+	# 		res.append((r['id'],r['zip']))
+	# 	return res
 
 	def getTownCountyWithPostcode(self,cr,uid,post,context):
 		postcode_id = self.search(cr, uid, [('id','=',post)], context=context)
 		obj = self.browse(cr,uid,postcode_id)
-		print obj
+		print dir(obj[0])
 		if obj != None:
-			return {'town':obj[0].town,'county':obj[0].county}
+			return {'town':obj[0].town,'state_id':obj[0]._id}
 
 	def getAllPostCodes(self,cr,uid,context=None):
 		all_record_ids = self.search(cr, uid,[])
 		all_records = self.browse(cr,uid,all_record_ids)
-		return [(r.id,r.postcode) for r in all_records]
+		return [(r.id,r.zip) for r in all_records]
+
+	def getPostCodes(self,cr,uid,post,context=None):
+		all_record_ids = search(cr, uid, [('id','=',post)], context=context)
+		all_records = self.browse(cr,uid,all_record_ids)
+		return [(r.id,r.zip) for r in all_records]
+
+	def getCounty(self,cr,uid,post,context=None):
+		all_record_ids = search(cr, uid, [('id','=',post)], context=context)
+		all_records = self.browse(cr,uid,all_record_ids)
+		return [(r.id,r.state_id) for r in all_records]
+
+	def getAllCounties(self,cr,uid,context=None):
+		all_record_ids = self.search(cr, uid,[])
+		all_records = self.browse(cr,uid,all_record_ids)
+		return [(r.id,r.state_id) for r in all_records]
 
 	_default = {
 		'x':0,
